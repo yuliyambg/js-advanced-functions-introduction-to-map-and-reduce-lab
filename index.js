dc6f04c96f1d28ca5d3e369a43d8cbb5bd46069c
yuliyambg <yuliyambg@gmail.com>
2021-02-19 20:54:51 -0500

    commit tree c5e20420eaa277d4f3fc3a88908f8efb6a9b8188

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+    return sourceArray.map(i => i *(-1));
+}
+
+function mapToNoChange(sourceArray) {
+    return sourceArray
+}
+
+function mapToDouble(sourceArray) {
+return sourceArray.map(i => i * 2);
+}
+
+function mapToSquare(sourceArray) {
+    return sourceArray.map(i => i**2)
+}
+
+function reduceToTotal(sourceArray, startingPoint=0) {
+   let total = startingPoint
+    for (let i of sourceArray) {
+        total += i
+    }
+    return total;
+}
+
+function reduceToAllTrue(sourceArray) {
+    for (let i of sourceArray) {
+        if (!i) return false;
+    }
+    return true;
+}
+
+function reduceToAnyTrue(sourceArray) {
+    for (let i of sourceArray) {
+        if (i) return true;
+    }
+    return false;
+}
